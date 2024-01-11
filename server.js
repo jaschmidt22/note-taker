c4154d1aeb651f258e47ce8351cd10b29c46ae2b
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-01-10 20:02:25 -0700

    updated route

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const express = require("express");
 const fs = require("fs");
 const uuid = require("uuid");
-const path = require("path");
-const htmlRoutes = require("./routes/htmlRoutes"); //import HTML routes
-const apiRoutes = require("./routes/apiRoutes"); //import API routes
+//const path = require("path");
+//const htmlRoutes = require("./routes/htmlRoutes"); //import HTML routes
+//const apiRoutes = require("./routes/apiRoutes"); //import API routes
 
 const PORT = 3001;
 
@@ -12,11 +12,38 @@ const PORT = 3001;
 const app = express();
 
 //configure middleware to hanlde JSON and static files
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-app.use("/html", htmlRoutes);
-app.use("/api", apiRoutes);
+// Serve the index.html file for all other routes (wildcard)
+//app.get("*", (req, res) => {
+// res.sendFile(__dirname + "/public/index.html");
+//});
+
+// Serve the notes.html file when the user accesses "/notes"
+//app.get("/notes", (req, res) => {
+//res.sendFile(__dirname + "/public/notes.html");
+//});
+
+//GET route to read and return all saved notes as JSON
+app.get("/api/notes", (req, res) => {
+  const notesData = JSON.parse(fs.readFileSync("db.json", "utf-8"));
+  res.json(notesData);
+});
+
+//POST route to receive a new note, add it to db.json, and return new note
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid.v4(); //generate unique ID for new note
+  const notesData = JSON.parse(fs.readFileSync("db.json", "utf-8"));
+  notesData.push(newNote);
+  fs.writeFileSync("db.json", JSON.stringify(notesData, null, 2));
+  res.json(newNote);
+});
+
+//app.use("/html", htmlRoutes);
+//app.use("/api", apiRoutes);
 
 //listen on specific port for incoming HTTP requests
 app.listen(PORT, () =>
